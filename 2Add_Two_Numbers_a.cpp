9db34c20c8d1fcd8a173147cec6e3cecf0e4429e
Mao <95645447+Mao0205@users.noreply.github.com>
2024-01-11 02:09:33 +0800

    Update 2Add_Two_Numbers_a.cpp

--- 2Add_Two_Numbers_a.cpp
+++ 2Add_Two_Numbers_a.cpp
@@ -6,78 +6,39 @@
 ListNode* temp = new ListNode(val);
 */
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode * ans = new ListNode();
-        ListNode * index;
+        ListNode* ans = new ListNode();
+        ListNode* index = ans;
         int val, flag = 0;
-        index = ans;
-        while(l1 != nullptr && l2!=nullptr){
-            val = l1->val+l2->val;
-            if(flag == 1){
-                val++;
-                flag = 0;
-            }
-            if(val >= 10){
-                flag = 1;
-                val = val%10;
-            }
-            ListNode* temp = new ListNode(val);
-            index->next = temp;
+        while (l1 != nullptr && l2 != nullptr) {
+            val = (l1->val + l2->val + flag) % 10;
+            flag = (l1->val + l2->val + flag) / 10;
+            index->next = new ListNode(val);
             index = index->next;
-
             l1 = l1->next;
             l2 = l2->next;
         }
-        ListNode* temp;
-        if(l1 != nullptr){
-            if(flag == 1){
-                l1->val = l1->val + 1;
-                temp = l1;
-                while(temp->val>=10){
-                    temp->val = 0;
-                    if(temp->next != nullptr)
-                        (temp->next)->val ++;
-                    else{
-                        ListNode* temp1 = new ListNode(1);
-                        temp->next = temp1;
-                    }
-                    temp = temp->next;
-                }
+        if (l1 != nullptr) {
+            while (l1 != nullptr) {
+                val = (l1->val + flag) % 10;
+                flag = (l1->val + flag) / 10;
+                index->next = new ListNode(val);
+                index = index->next;
+                l1 = l1->next;
             }
-            index->next = l1;
-        }else if(l2 != nullptr){
-            if(flag == 1){
-                l2->val = l2->val+1;
-                temp = l2;
-                while(temp->val>=10){
-                    temp->val = 0;
-                    if(temp->next != nullptr)
-                        (temp->next)->val ++;
-                    else{
-                        ListNode* temp1 = new ListNode(1);
-                        temp->next = temp1;
-                    }
-                    temp = temp->next;
-                }
+        } else {
+            while (l2 != nullptr) {
+                val = (l2->val + flag) % 10;
+                flag = (l2->val + flag) / 10;
+                index->next = new ListNode(val);
+                index = index->next;
+                l2 = l2->next;
             }
-            index->next = l2;
-        }else{
-            if(flag == 1){
-                ListNode* temp = new ListNode(1);
-                index->next = temp;
-            }
+        }
+        if (flag == 1) {
+            index->next = new ListNode(1);
         }
         return ans->next;
     }
